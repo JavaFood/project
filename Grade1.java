dcf8ff7b268d7eb1c802f4c171b322949f83d0c2
JavaFood <68105873+JavaFood@users.noreply.github.com>
2020-07-15 17:01:56 +0900

    Update Grade1.java

--- Grade1.java
+++ Grade1.java
@@ -1,5 +1,4 @@
 package UI;
-
 import java.awt.*;
 
 import javax.swing.*;
@@ -50,25 +49,25 @@ public class Grade1 extends JFrame {
 		//setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel Home = new JLabel("È¸»çÀÌ¸§ / Æò±Õ ÆòÁ¡");
+		JLabel Home = new JLabel("íšŒì‚¬ì´ë¦„ / í‰ê·  í‰ì ");
 		Home.setBounds(24, 77, 250, 65);
-		Home.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 21));
+		Home.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.BOLD, 21));
 		contentPane.add(Home);
 
-		JLabel lblNewLabel = new JLabel("ÆòÁ¡ & ÈÄ±â");
+		JLabel lblNewLabel = new JLabel("í‰ì  & í›„ê¸°");
 		lblNewLabel.setBounds(125, 12, 139, 34);
 		contentPane.add(lblNewLabel);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 25));
+		lblNewLabel.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.BOLD, 25));
 		lblNewLabel.setForeground(Color.WHITE);
 
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(24, 142, 111, 44);
 		comboBox.setBackground(Color.WHITE);
 		comboBox.setForeground(Color.BLACK);
-		comboBox.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 15));
+		comboBox.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.BOLD, 15));
 		comboBox.setModel(
-				new DefaultComboBoxModel(new String[] { "È¸»çº°º¸±â", "Tasty", "ÁÖ½Ä", "¹°»ê", "Costca", "Riotgames" }));
+				new DefaultComboBoxModel(new String[] { "íšŒì‚¬ë³„ë³´ê¸°", "Tasty", "ì£¼ì‹", "ë¬¼ì‚°", "Costca", "Riotgames" }));
 		contentPane.add(comboBox);
 
 		JComboBox comboBox_1 = new JComboBox();
@@ -78,14 +77,14 @@ public class Grade1 extends JFrame {
 		comboBox_1.setModel(new DefaultComboBoxModel(new String[] { "\uBCF4\uAE30 \uC21C\uC11C",
 				"\uB192\uC740 \uD3C9\uC810 \uC21C", "\uB0AE\uC740 \uD3C9\uC810 \uC21C" }));
 		comboBox_1.setForeground(Color.BLACK);
-		comboBox_1.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 15));
+		comboBox_1.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.BOLD, 15));
 		comboBox_1.setBackground(Color.WHITE);
 		contentPane.add(comboBox_1);
 		
 		//JPanel TablePanel=new JPanel();
 		//TablePanel.setBounds(24,198,326,155);
 		
-		DefaultTableModel dtm=new DefaultTableModel(new String[] {"È¸»çÀÌ¸§","ÈÄ±â","ÀÛ¼ºÀÚ","ÆòÁ¡"},0);
+		DefaultTableModel dtm=new DefaultTableModel(new String[] {"íšŒì‚¬ì´ë¦„","í›„ê¸°","ì‘ì„±ì","í‰ì "},0);
 		JTable table=new JTable(dtm);
 		table.setBounds(0,0,300,200);
 
@@ -96,7 +95,7 @@ public class Grade1 extends JFrame {
 		contentPane.add(scp);
 //		
 //		table_1 = new JTable(dtm);
-//		table_1.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 13));
+//		table_1.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.BOLD, 13));
 //		JScrollPane scrollPane = new JScrollPane(table_1,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 ////		JScrollPane scrollPane=new JScrollPane();
 //		scrollPane.setBounds(24, 198, 326, 155);
@@ -108,7 +107,7 @@ public class Grade1 extends JFrame {
 //		table_1.setPreferredSize(new Dimension(300,155));
 //		table_1.setModel(new DefaultTableModel(
 //			new Object[][] {
-//				{"   È¸»çÀÌ¸§", "     ÈÄ±â", "    ÀÛ¼ºÀÚ", "     ÆòÁ¡"},
+//				{"   íšŒì‚¬ì´ë¦„", "     í›„ê¸°", "    ì‘ì„±ì", "     í‰ì "},
 //			},
 //			new String[] {
 //				"Cname", "Text", "Name", "Number"
@@ -128,7 +127,7 @@ public class Grade1 extends JFrame {
 //		comboBox_2.setModel(new DefaultComboBoxModel(new String[] { "\uBCF4\uAE30 \uC21C\uC11C",
 //				"\uB192\uC740 \uD3C9\uC810 \uC21C", "\uB0AE\uC740 \uD3C9\uC810 \uC21C" }));
 //		comboBox_2.setForeground(Color.BLACK);
-//		comboBox_2.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 15));
+//		comboBox_2.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.BOLD, 15));
 //		comboBox_2.setBackground(Color.WHITE);
 //		contentPane.add(comboBox_2);
 
@@ -136,23 +135,23 @@ public class Grade1 extends JFrame {
 		JComboBox CnameT = new JComboBox();
 		CnameT.setBackground(Color.WHITE);
 		CnameT.setForeground(Color.BLACK);
-		CnameT.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 15));
+		CnameT.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.BOLD, 15));
 		CnameT.setModel(
-				new DefaultComboBoxModel(new String[] { "", "Tasty", "ÁÖ½Ä", "¹°»ê", "Costca", "Riotgames" }));
+				new DefaultComboBoxModel(new String[] { "", "Tasty", "ì£¼ì‹", "ë¬¼ì‚°", "Costca", "Riotgames" }));
 		CnameT.setBounds(24, 365, 101, 46);
 		contentPane.add(CnameT);
 
 		JComboBox NumberT = new JComboBox();
 		NumberT.setBackground(Color.WHITE);
 		NumberT.setForeground(Color.BLACK);
-		NumberT.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 15));
+		NumberT.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.BOLD, 15));
 		NumberT.setModel(
 				new DefaultComboBoxModel(new String[] { "0.0","0.5","1.0","1.5","2.0","2.5","3.0","3.5","4.0","4.5","5.0"}));
 		NumberT.setBounds(134, 365, 101, 46);
 		contentPane.add(NumberT);
 		
 		NameT = new JTextField();
-		NameT.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 15));
+		NameT.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.BOLD, 15));
 		NameT.setHorizontalAlignment(SwingConstants.CENTER);
 		NameT.setText("\uC791\uC131\uC790");
 		NameT.setBounds(249, 365, 101, 46);
@@ -162,30 +161,36 @@ public class Grade1 extends JFrame {
 		
 
 		TextInput = new JTextField();
-		TextInput.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 15));
+		TextInput.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.BOLD, 15));
 		TextInput.setHorizontalAlignment(SwingConstants.CENTER);
 		TextInput.setText("\uD6C4\uAE30\uB97C \uC785\uB825\uD558\uC138\uC694");
 		TextInput.setBounds(24, 421, 326, 101);
 		contentPane.add(TextInput);
 		TextInput.setColumns(10);
 
-		JButton btnNewButton = new JButton("ÀÛ¼º");
+		JButton btnNewButton = new JButton("ì‘ì„±");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				if(NameT.getText().equals("ì‘ì„±ì")&&TextInput.getText().equals("í›„ê¸°ë¥¼ ì…ë ¥í•˜ì„¸ìš”")) {
+					JOptionPane.showMessageDialog(null, "ì…ë ¥ê°’ì„ í™•ì¸í•˜ì„¸ìš”.");
+					return ;
+					}
 				DefaultTableModel model = (DefaultTableModel) table.getModel();
 				model.addRow(
 						new Object[] { CnameT.getSelectedItem().toString(), TextInput.getText(), NameT.getText(), NumberT.getSelectedItem().toString() });
+				NameT.setText("ì‘ì„±ì");
+				TextInput.setText("í›„ê¸°ë¥¼ ì…ë ¥í•˜ì„¸ìš”");
 			}
 		});
 		btnNewButton.setBounds(276, 157, 74, 29);
 		btnNewButton.setBackground(new Color(230, 230, 250));
-		btnNewButton.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 15));
+		btnNewButton.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.BOLD, 15));
 		contentPane.add(btnNewButton);
 
 		JButton HomeBack = new JButton("New button");
-		HomeBack.setIcon(new ImageIcon("Áı1.PNG"));
+		HomeBack.setIcon(new ImageIcon("ì§‘1.PNG"));
 		HomeBack.setBounds(325, 13, 43, 44);
-		HomeBack.setPressedIcon(new ImageIcon("Áı2.PNG"));
+		HomeBack.setPressedIcon(new ImageIcon("ì§‘2.PNG"));
 		contentPane.add(HomeBack);
 		HomeBack.addActionListener(new ActionListener() {
 
@@ -198,9 +203,9 @@ public class Grade1 extends JFrame {
 		});
 
 		JButton Back = new JButton("New button");
-		Back.setIcon(new ImageIcon("È­»ìÇ¥1.png"));
+		Back.setIcon(new ImageIcon("í™”ì‚´í‘œ1.png"));
 		Back.setBounds(24, 22, 43, 27);
-		Back.setPressedIcon(new ImageIcon("È­»ìÇ¥2.PNG"));
+		Back.setPressedIcon(new ImageIcon("í™”ì‚´í‘œ2.PNG"));
 		contentPane.add(Back);
 
 		JPanel panel = new JPanel();
